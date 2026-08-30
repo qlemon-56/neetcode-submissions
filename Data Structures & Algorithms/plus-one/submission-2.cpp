544d62733534998c9ad6e4a00655044c90bc8bed
Mark <75143899+qlemon-56@users.noreply.github.com>
2026-08-31 00:37:21 +0100

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        auto it = prev(digits.end());
+        int power {0}, num{0}, carry{0};
+        do {
+            int res = ++(*it);
+            if (res==10) {
+                *it = 0;
+                carry = 1;
+            } else carry = 0;
+            it--;
+            
+        } while (carry!=0 && it>=digits.begin());
+        if (carry!=0) digits.insert(digits.begin(), 1);
+        
+        return digits;
+    }
+};
